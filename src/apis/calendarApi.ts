51081565c2a8068f8b8639812d97032066825b7d
Jared Finn <jaredhfinn@gmail.com>
2021-10-11 16:33:18 -0400

    new build, 10/11, 4:33

--- src/apis/calendarApi.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const calendarApi = axios.create({
-    baseURL: 'https://www.googleapis.com/calendar/v3',
-    params: {
-        key: 'AIzaSyAHwsH4Yvosm9BvxfmOqklmuqmiNO6QuqQ',
-    },
-});
