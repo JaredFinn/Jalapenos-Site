c03b146d7d0b5679abe7ae83c26b4e3eabfc51bd
Jared Finn <jaredhfinn@gmail.com>
2021-10-28 12:45:33 -0400

    Navbar updates and link image change

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -29,8 +29,8 @@ export const Navbar: React.FC = () => {
                 <h1>Los Jalapenos</h1>
             </div>
             <div className="nav-center">
-                 <a className="number" href='tel:+18452559000'>(845) 255-9000</a>
-                
+                <h3 className="mobile-title">Los Jalapenos</h3>
+                <a className="number" href='tel:+18452559000'>(845) 255-9000</a>
             </div>
             <div className="nav-right">
                 <ul className="nav-list">
@@ -55,13 +55,15 @@ export const Navbar: React.FC = () => {
             </div>
             <div className={isOpen ? 'nav-right-mobile-menu active': 'nav-right-mobile-menu'}>
                 <a onClick={() => close()} className='closebtn'>&times;</a>
-                <a href="https://scontent-lga3-2.xx.fbcdn.net/v/t1.6435-9/120562035_1699028470256235_3802361383676925407_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=973b4a&_nc_ohc=OaYYUg5DVdIAX-0F4bJ&_nc_ht=scontent-lga3-2.xx&oh=79fa33b830880fddfdfe6611ad0291b4&oe=618A8649">Menu</a>
-                <a href='#location'
-                    onClick={(e) => {
-                        close()
-                        e.preventDefault();
-                        window.document.getElementById('location')?.scrollIntoView();
-                    }}>Location</a>
+                <div className="links">
+                    <a href="https://scontent-lga3-2.xx.fbcdn.net/v/t1.6435-9/120562035_1699028470256235_3802361383676925407_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=973b4a&_nc_ohc=OaYYUg5DVdIAX-0F4bJ&_nc_ht=scontent-lga3-2.xx&oh=79fa33b830880fddfdfe6611ad0291b4&oe=618A8649">Menu</a>
+                    <a href='#location'
+                        onClick={(e) => {
+                            close()
+                            e.preventDefault();
+                            window.document.getElementById('location')?.scrollIntoView();
+                        }}>Location</a>
+                </div>
             </div>
         </nav>
     );
